18a2d1390bab53b7a0d29dca93300c605db5e967
amoresca <casee.amores@gmail.com>
2021-01-13 15:03:22 -0500

    adds some starter products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(
+  [
+    {
+      name: "2021 Calendar",
+      price: 20.00,
+      description: "wall calendar with a cute animal for each month"
+    },
+    {
+      name: "Mini Dog Stickets",
+      price: 4.50,
+      description: "sheet of small, assorted dogs (small stickers, not small dog breeds!)"
+    },
+    {
+      name: "Cat Eraser",
+      price: 3.00,
+      description: "eraser shaped like a cat"
+    }
+  ]
+)
