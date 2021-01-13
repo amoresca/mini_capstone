0a83df8257a6ead1f1f57d4f3c66208d0930e0eb
amoresca <casee.amores@gmail.com>
2021-01-13 15:09:09 -0500

    adds product images

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,19 @@ Product.create(
     {
       name: "2021 Calendar",
       price: 20.00,
+      image_url: "https://i2.wp.com/thecottagemarket.com/wp-content/uploads/2020/08/TCM-Watercolor-Animal-2021-1.jpg"
       description: "wall calendar with a cute animal for each month"
     },
     {
       name: "Mini Dog Stickets",
       price: 4.50,
+      image_url: "https://i.etsystatic.com/19765352/r/il/09e60d/1922301506/il_570xN.1922301506_fkj4.jpg"
       description: "sheet of small, assorted dogs (small stickers, not small dog breeds!)"
     },
     {
       name: "Cat Eraser",
       price: 3.00,
+      image_url: "https://kawaii.kawaii.at/images/product_images/big_images/blue-Lucky-Cat-eraser-Maneki-Neko-from-Japan-206876-1.JP"
       description: "eraser shaped like a cat"
     }
   ]
